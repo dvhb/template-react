36b6a7751f1d2a3f0e39ddb9e4a195edf021de3d
Aleksey Subbotenko <asu@dvhb.ru>
2020-08-12 13:10:46 +0300

    fix(ci): cypress

--- template/tools/cypress/cypress-open.js
+++ template/tools/cypress/cypress-open.js
@@ -1,6 +1,6 @@
 const cypress = require('cypress');
 
-const cliEnvType = process.argv[2] || 'dev'; // local/dev/stage
+const cliEnvType = process.argv[2] || 'local'; // local/dev/stage
 
 const baseUrl = cliEnvType === 'local' ? 'http://localhost:3000' : `https://${cliEnvType}.ru`;
 
